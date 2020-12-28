8469d3e39b52e9c020b27db6293dfc2ece7086f4
Aldo Paganica <s1092434@studenti.univpm.it>
2020-12-28 15:42:19 +0100

    aggiunto archivio

--- weather/src/main/java/it/mustillopaganica/weather/WeatherApplication.java
+++ weather/src/main/java/it/mustillopaganica/weather/WeatherApplication.java
@@ -2,12 +2,18 @@ package it.mustillopaganica.weather;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import it.mustillopaganica.weather.utilities.Archivio;
 
 @SpringBootApplication
+@EnableScheduling
 public class WeatherApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WeatherApplication.class, args);
+		Archivio.popola();
+		Archivio.scriviForecast();
 	}
 
 }
---
+++ weather/src/main/java/it/mustillopaganica/weather/utilities/Archivio.java
@@ -0,0 +1,67 @@
+package it.mustillopaganica.weather.utilities;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Vector;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import it.mustillopaganica.weather.model.Data;
+import it.mustillopaganica.weather.service.MeteoParser;
+
+/**
+ * @author rbtms
+ * @author aldop
+ * @Component annotazione che definisce la classe come componente autogestito da Spring
+ */
+@Component
+public class Archivio {
+	
+public static Vector<String> citta = new Vector<String>();
+	
+	@SuppressWarnings("resource")
+	public static Vector<String> popola(){
+		
+		try {
+			
+			BufferedReader read = new BufferedReader(new FileReader("citta.txt"));
+			
+			String city = read.readLine();
+			while(city != null) {
+				citta.add(city);
+				city=read.readLine();
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return citta;
+	}
+	
+	public static void ScriviFile(Vector<Data> v, String nomefile) {
+		try{
+			BufferedWriter out =
+					new BufferedWriter (new FileWriter (nomefile+".txt"));
+			out.write(v.toString());
+			out.close();
+			}catch (Exception ex){
+				ex.printStackTrace();;
+			}
+        }
+	
+	@Scheduled(fixedRate=10080*60*1000)
+	public static void scriviForecast() {
+		for(String c: citta) {
+			MeteoParser m = new MeteoParser(c);
+			m.parser();
+			ScriviFile(m.getArr(),"archivio");
+		}
+	}
+	
+	
+}
